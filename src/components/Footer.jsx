886d4dd1a57cdb5060fa198292dc87d1a135875a
Hanisha Bundela <bundelahanisha25@gmail.com>
2023-03-03 13:00:03 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../img/logo.png'
+const Footer = () => {
+    return(
+       <footer>
+            <img src =  {Logo} alt = ""></img>
+            <span>Made with dedication and react js</span>
+       </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Login = () =>{
+    return(
+        <div className = 'auth'>
+            <h1>Login</h1>
+            <form>
+                <input required type= 'text' placeholder='UserName'></input>
+                <input required type= 'password' placeholder='Password'></input>
+                <button>Login</button>
+                <span>Don't have an account?<Link to = "/register">Register</Link>
+                </span>
+            </form>
+
+
+        </div>
+    );
+}
+export default Login;
